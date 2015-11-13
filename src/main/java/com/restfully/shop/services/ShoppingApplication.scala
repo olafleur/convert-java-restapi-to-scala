552e33c55e2120cae317273362a4eb96f810f63b
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-11-13 16:08:16 -0500

    Fix ShoppingApplication

--- src/main/java/com/restfully/shop/services/ShoppingApplication.scala
+++ src/main/java/com/restfully/shop/services/ShoppingApplication.scala
@@ -1,24 +1,12 @@
-package com.restfully.shop.services;
+package com.restfully.shop.services
 
-import javax.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
+import javax.ws.rs.core.Application
 
-public class ShoppingApplication extends Application {
-   private Set<Object> singletons = new HashSet<>();
-   private Set<Class<?>> empty = new HashSet<>();
+import scala.collection.immutable.HashSet
 
-   public ShoppingApplication() {
-      singletons.add(new CustomerResource());
-   }
+class ShoppingApplication extends Application {
+   var singletons = HashSet[Object]()
+   val empty = HashSet[Class]()
 
-   @Override
-   public Set<Class<?>> getClasses() {
-      return empty;
-   }
-
-   @Override
-   public Set<Object> getSingletons() {
-      return singletons;
-   }
+   singletons = singletons + new CustomerResource
 }
